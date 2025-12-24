ccdf7a92c87639717036072d253c51fdba969933
Tejas <tejasspatell2@gmail.com>
2025-12-24 19:47:59 +0530

    learning string

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const client = require("./client.js")
 
 async function init(){
 
+    //? String
+    await client.set("number:1", "1")
+    await client.set("number:2", "2")
+    await client.set("number:3", "3")
+    await client.set("number:4", "4")
+    await client.setnx("number:4", "hello")   //? store only when key not exist
 
-
-
-
-
-
-
+    console.log("learning string -> ", await client.get("number:2"))
 
 
 
@@ -25,6 +26,10 @@ async function init(){
     console.log("Sorted Set value is -> ", await client.zrange('rank', 0, -1))
     console.log("Sorted Set reverse value is -> ", await client.zrevrange('rank', 0, -1))
     console.log("Access value -> ", await client.zrank("rank", "anil"))
+
+    //? stream-
+    await client.xadd('temp', "*", "orderid", "123", "userid", "456", "status", "done")
+
 
 }
 
